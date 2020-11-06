ca6b19407b2e7bf2d36d0fa1712f1899a626d4df
Ponganesh P <abcdpons@gmail.com>
2020-11-06 18:43:32 +0530

    Updates

---
+++ precache-manifest.5471384b0caa06a16413206ac25f3f3b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e85fbe4e1d068e1fe08d0a2e1dc41276",
+    "url": "/PGs_resume/index.html"
+  },
+  {
+    "revision": "92e861a8931a7a98e430",
+    "url": "/PGs_resume/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "4a0ef258a75d857e2421",
+    "url": "/PGs_resume/static/js/2.79f9e1be.chunk.js"
+  },
+  {
+    "revision": "f5fc2afae58c4ffc30c6148864765d4b",
+    "url": "/PGs_resume/static/js/2.79f9e1be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92e861a8931a7a98e430",
+    "url": "/PGs_resume/static/js/main.eeb67bd8.chunk.js"
+  },
+  {
+    "revision": "65ec762f4ebfd8772fda",
+    "url": "/PGs_resume/static/js/runtime-main.b815f2ed.js"
+  }
+]);
